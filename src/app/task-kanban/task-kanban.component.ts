1027894be4890f69cc32fcb5263f90daa91dc690
Brittany Mazza <bmazza555@gmail.com>
2017-05-26 09:14:14 -0700

    Add task-kanban component

---
+++ src/app/task-kanban/task-kanban.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-kanban',
+  templateUrl: './task-kanban.component.html',
+  styleUrls: ['./task-kanban.component.css']
+})
+export class TaskKanbanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
