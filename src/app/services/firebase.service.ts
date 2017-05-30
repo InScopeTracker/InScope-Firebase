b225e9bd7d1e38a8b0ee3a7719c0e7f4a058596b
Brittany Mazza <bmazza555@gmail.com>
2017-05-29 20:11:01 -0700

    Reformat file to match the rest of the project

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,79 +1,79 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 
 @Injectable()
 export class FirebaseService {
-    projects: FirebaseListObservable<any[]>;
-    tasks: FirebaseListObservable<any[]>;
-    project: FirebaseObjectObservable<any>;
-    task: FirebaseObjectObservable<any>;
-    authToken: any;
+  projects: FirebaseListObservable<any[]>;
+  tasks: FirebaseListObservable<any[]>;
+  project: FirebaseObjectObservable<any>;
+  task: FirebaseObjectObservable<any>;
+  authToken: any;
 
-    constructor(private af: AngularFire) {
-        this.af.auth.subscribe(auth => {
-            if (auth) {
-                this.authToken = auth;
-            }
-        });
-    }
+  constructor(private af: AngularFire) {
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.authToken = auth;
+      }
+    });
+  }
 
-    getProjects() {
-        this.projects = this.af.database.list('/projects', {
-            query: {
-                orderByChild: 'owner',
-                equalTo: this.authToken.auth.email,
-            }
-        }) as FirebaseListObservable<Project[]>;
-        return this.projects;
-    }
+  getProjects() {
+    this.projects = this.af.database.list('/projects', {
+      query: {
+        orderByChild: 'owner',
+        equalTo: this.authToken.auth.email,
+      }
+    }) as FirebaseListObservable<Project[]>;
+    return this.projects;
+  }
 
-    getProject(id) {
-        this.project = this.af.database.object('/projects/' + id) as FirebaseObjectObservable<Project>;
-        return this.project;
-    }
+  getProject(id) {
+    this.project = this.af.database.object('/projects/' + id) as FirebaseObjectObservable<Project>;
+    return this.project;
+  }
 
-    getTasks(projectId) {
-        this.tasks = this.af.database.list('/tasks', {
-            query: {
-                orderByChild: 'projectId',
-                equalTo: projectId,
-            }
-        }) as FirebaseListObservable<Task[]>;
-        return this.tasks;
-    }
+  getTasks(projectId) {
+    this.tasks = this.af.database.list('/tasks', {
+      query: {
+        orderByChild: 'projectId',
+        equalTo: projectId,
+      }
+    }) as FirebaseListObservable<Task[]>;
+    return this.tasks;
+  }
 
-    completeTask(taskKey: string) {
-        this.af.database.object('/tasks/' + taskKey).remove();
-    }
+  completeTask(taskKey: string) {
+    this.af.database.object('/tasks/' + taskKey).remove();
+  }
 
-    deleteProject(projectKey: string) {
-        this.af.database.list('/tasks', {
-            query: {
-                orderByChild: 'projectId',
-                equalTo: projectKey
-            }
-        }).subscribe(res => {
-            const key = res[0].$key;
-            this.af.database.object('/tasks/' + key).remove();
-        });
+  deleteProject(projectKey: string) {
+    this.af.database.list('/tasks', {
+      query: {
+        orderByChild: 'projectId',
+        equalTo: projectKey
+      }
+    }).subscribe(res => {
+      const key = res[0].$key;
+      this.af.database.object('/tasks/' + key).remove();
+    });
 
 
-        this.af.database.object('/projects/' + projectKey).remove();
-    }
+    this.af.database.object('/projects/' + projectKey).remove();
+  }
 }
 
 interface Project {
-    $key?: string;
-    title?: string;
-    owner?: string;
-    timestamp: Date;
+  $key?: string;
+  title?: string;
+  owner?: string;
+  timestamp: Date;
 }
 
 interface Task {
-    $key?: string;
-    title?: string;
-    owner?: string;
-    projectTitle?: string;
-    projectId: string;
-    timestamp: Date;
+  $key?: string;
+  title?: string;
+  owner?: string;
+  projectTitle?: string;
+  projectId: string;
+  timestamp: Date;
 }
