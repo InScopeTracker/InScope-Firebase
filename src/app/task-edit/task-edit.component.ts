2a80c9d141906f80406f985203430ab42e25332d
Brittany Mazza <bmazza555@gmail.com>
2017-05-30 23:01:23 -0700

    Create task with edit task form (update task is a WIP)

--- src/app/task-edit/task-edit.component.ts
+++ src/app/task-edit/task-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 import { AppComponent } from '../app.component';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FirebaseService }  from '../services/firebase.service';
+import { FirebaseService } from '../services/firebase.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-task-edit',
@@ -15,10 +16,13 @@ export class TaskEditComponent implements OnInit {
   public newTask: string;
   public currentProjectId: string;
   public currentProject: any;
+  public task: FirebaseObjectObservable<any>;
+  form: FormGroup;
 
   constructor(private firebaseService: FirebaseService,
               public app: AppComponent,
               public af: AngularFire,
+              public fb: FormBuilder,
               private router: Router,
               private route: ActivatedRoute) {
     this.af.auth.subscribe(auth => {
@@ -34,6 +38,38 @@ export class TaskEditComponent implements OnInit {
         this.currentProject = project;
     });
     this.tasks = this.firebaseService.getTasks(this.currentProjectId);
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      description: ''
+    });
+    this.form.setValue({
+      name: '',
+      description: '(this currently does nothing)'
+    });
+    this.firebaseService.getTask(this.route.parent.snapshot.params['id']).subscribe(task => {
+      this.task = task;
+
+      // TODO: Setting values here is not working - we want to populate from with existing task
+      this.form.setValue({
+        name: task.name,
+        description: ''
+      });
+    });
+  }
+
+  onSubmit(form) {
+    // TODO: Update an existing task
+    // For now, this only creates a new task.
+    const task = {
+      title: form.get('name').value,
+      owner: this.authToken.auth.email,
+      projectTitle: this.currentProject.title,
+      projectId: this.currentProjectId,
+      timestamp: Date.now()
+    };
+    this.tasks.push(task).then(() => {
+      this.router.navigateByUrl('/project/' + this.currentProjectId + '/task/list');
+    });
   }
 
   createTask() {
