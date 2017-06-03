785f555855b660db815869e48679d9202252948e
c-pina <cpina@christosmbp2015.attlocal.net>
2017-06-03 14:27:50 -0700

    Kanban board shows tasks by status

--- src/app/task-kanban/task-kanban.component.ts
+++ src/app/task-kanban/task-kanban.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { FirebaseService } from '../services/firebase.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-task-kanban',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-kanban.component.css']
 })
 export class TaskKanbanComponent implements OnInit {
+  authToken: any;
+  public tasks: FirebaseListObservable<any>;
+  public currentProjectId: any;
+  statuses = ['To-Do', 'Delegated', 'Doing'];
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService,
+              public af: AngularFire,
+              private route: ActivatedRoute) {
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.authToken = auth;
+      }
+    });
+  }
 
   ngOnInit() {
+    this.currentProjectId = this.route.snapshot.parent.parent.params['id'];
+    this.tasks = this.firebaseService.getTasks(this.currentProjectId); 
+  }
+
+  completeTask(task: any) {
+    this.firebaseService.completeTask(task.$key);
   }
 
 }
