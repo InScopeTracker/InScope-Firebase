caf51abacd42b651e4904b66710b0b3387075898
c-pina <[christopher.pina@yahoo.com]>
2017-06-07 23:18:21 -0700

    Hides project settings for projects not owned by user

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -3,6 +3,7 @@ import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/d
 import { ActivatedRoute } from '@angular/router';
 import { AppComponent } from '../app.component';
 import { FirebaseService } from '../services/firebase.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-project',
@@ -11,15 +12,18 @@ import { FirebaseService } from '../services/firebase.service';
 })
 
 export class ProjectComponent implements OnInit {
+  public user: FirebaseObjectObservable<any>;
   public tasks: FirebaseListObservable<any>;
   public currentProjectId: any;
   public currentProject: FirebaseObjectObservable<any>;
 
   constructor(private firebaseService: FirebaseService,
               public app: AppComponent,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute, 
+              private authService: AuthService ) { }
 
   ngOnInit() {
+    this.user = this.firebaseService.getUser(this.authService.user.uid);
     this.currentProjectId = this.route.snapshot.params['id'];
     this.currentProject = this.firebaseService.getProject(this.currentProjectId);
     this.tasks = this.firebaseService.getTasks(this.currentProjectId);
