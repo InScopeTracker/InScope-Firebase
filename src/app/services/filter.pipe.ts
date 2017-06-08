6e4e060013d10d5d6b6ad91d316b957b6a69cac8
c-pina <[christopher.pina@yahoo.com]>
2017-06-08 00:14:28 -0700

    Projects now filter by owned and owned by others on home screen.

--- src/app/services/filter.pipe.ts
+++ src/app/services/filter.pipe.ts
@@ -2,11 +2,18 @@ import { Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({name: 'filter'})
 
+//Pipe filters array by the value of the filed indicated.
+//Setting filterOut parameter to true, filters out objects with passed value at passed field. 
 export class FilterPipe implements PipeTransform {
-    transform(items: any[], field : string, value : string): any {
+    transform(items: any[], field : string, value : string, filterOut: boolean): any {
         if (!items || !field) {
             return items;
         }
-        return items.filter(item => item[field] == value);
+
+        if(filterOut){
+            return items.filter(item => item[field] != value);
+        } else {
+            return items.filter(item => item[field] == value);
+        }
     }
 }
